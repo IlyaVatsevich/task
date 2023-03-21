5eececaa799232acaa170a49f34c4435d4b0e16e
Ilya Vatsevich <leone999@mail.ru>
2023-03-21 11:58:09 +0300

    initial commit

---
+++ src/main/java/by/ilya_vatsevich/bootcamp_task/BootcampTaskApplication.java
@@ -0,0 +1,13 @@
+package by.ilya_vatsevich.bootcamp_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/ilya_vatsevich/bootcamp_task/BootcampTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ilya_vatsevich.bootcamp_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
